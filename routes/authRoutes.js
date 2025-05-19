05611a8e76a9ced5f1fab62c7cbbf02ac1443696
Prayatna Rajakrnikar <prayatna.rajkarnikar@gmail.com>
2025-05-19 06:53:36 +0545

    fix: features

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,11 +4,13 @@ import {
   loginUser,
   editWeight,
 } from "../controllers/authController.js";
+import { getUserDailyReport } from "../controllers/reportController.js";
 
 const router = Router();
 router.post("/register", registerUser);
 
 router.post("/login", loginUser);
 router.put("/updateWeight/:userId", editWeight);
+router.get("/report/:userId", getUserDailyReport);
 
 export default router;
